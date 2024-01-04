d47d0f064325c9f78d4288f467921364eb6c72fb
aryan-kumar-mains <ak2010999@gmail.com>
2024-01-04 22:58:49 +0530

    day4 code added

---
+++ Day4/2870MinimumOptrToMakeArrayEmpty.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty/description/
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int ans = 0;
+        
+        unordered_map<int,int> f;
+        
+        for(auto i : nums)
+            ++f[i];
+        
+        for(auto i : f){
+            if(i.second % 3 == 0){
+                ans += i.second / 3;
+            }
+            else if(i.second % 3 == 2){
+                ++ans;
+                ans += (i.second - 2) / 3;
+            }
+            else{
+                if(i.second == 1)
+                    return -1;
+                
+                ans += 2;
+                ans += (i.second - 4) / 3;
+            }
+        }
+        
+        return ans;
+    }
+};
